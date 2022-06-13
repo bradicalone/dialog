3d289a766664a7c51ba79f7b2b108c0d18a0dc3f
bradicalone <b.vanderbush@yahoo.com>
2022-06-12 21:22:19 -0700

    no element fix

--- index.js
+++ index.js
@@ -244,6 +244,46 @@ class DialogVarify {
 }
 
 
+const dialogElement = document.querySelector('.Dialog#one')
+const anotherDialogElement = document.querySelector('.Dialog#two')
 
-    
+const dialogOne = new DialogVarify(dialogElement)
+const dialogTwo = new DialogVarify(anotherDialogElement)
+
+const targetsOne = Array.from(document.querySelectorAll('input.first'))
+const targetsTwo = Array.from(document.querySelectorAll('input.two'))
+
+targetsOne.forEach(target => {
+    target.addEventListener('click', (e) => dialogOne.open(e))
+    target.addEventListener('input', (e) => {
+        // Index of password or email check element
+        const {index, verfifyElement} = dialogOne.checkValidation(e)
+        console.log('verfifyElement:', verfifyElement)
+        if(index !== -1 && verfifyElement) {
+            verfifyElement.querySelectorAll('span')[index].classList.add('varify-active')
+        }
+    })
+})
+
+targetsTwo.forEach(target => {
+    target.addEventListener('click', (e) => dialogTwo.open(e)),
+    target.addEventListener('input', (e) => {
+        // Index of password or email check element
+        const {index, verfifyElement} = dialogTwo.checkValidation(e)
+        if(index !== -1 && verfifyElement) {
+            verfifyElement.querySelectorAll('span')[index].classList.add('varify-active')
+        }
+    })
+})
+
+
+const closeBtns = Array.from(document.querySelectorAll('.close-dialog'))
+closeBtns.forEach(close =>
+    close.addEventListener('click', (e) => {
+        if (e.target.id === 'close-one') {
+            return dialogOne.close(e)
+        }
+        dialogTwo.close(e)
+    })
+);
 
